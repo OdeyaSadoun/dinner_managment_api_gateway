e894b5bb293bb15a5cc39f2f62347e750de5a2f6
אודיה סעדון <osadoun@g.jct.ac.il>
2024-11-20 17:41:37 +0200

    delete unnccery file

--- src/globals/consts/person_data_const_strings.py
+++
@@ -1,9 +0,0 @@
-class PersonDataConstStrings:
-    # ? Keys
-    _id_key = "_id"
-    name_key = "name"
-    phone_key = "phone"
-    table_number_key = "table_number"
-    is_reach_the_dinner_key = "is_reach_the_dinner"
-    date_created_key = "date_created"
-    is_active_key = "is_active"
