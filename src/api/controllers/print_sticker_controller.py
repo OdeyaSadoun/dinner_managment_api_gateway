3a6de767d0abe84c6e63ebe1005bffe60314dc5f
אודיה סעדון <osadoun@g.jct.ac.il>
2025-05-05 21:37:31 +0300

    add emoty func for printing

--- src/api/controllers/print_sticker_controller.py
+++ src/api/controllers/print_sticker_controller.py
@@ -3,8 +3,5 @@ import sys
 
 
 class PrintStickerController:
-    def create_text_image(self, text: str, image_path: str):
-        pass
-
-    def print_sticker(self):
+    def print_sticker(self, text: str, image_path: str):
         pass
